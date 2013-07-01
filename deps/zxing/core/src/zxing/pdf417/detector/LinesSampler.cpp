3adc550ee9b23d55de2019a612472bfdf98cac30
Christoph Schulz <schulzcbs@gmail.com>
2013-07-01 15:37:15 +0200

    The usual ZXing symbol fix.

--- deps/zxing/core/src/zxing/pdf417/detector/LinesSampler.cpp
+++ deps/zxing/core/src/zxing/pdf417/detector/LinesSampler.cpp
@@ -35,10 +35,10 @@ using zxing::Point;
 // VC++
 using zxing::Line;
 
-//const int LinesSampler::MODULES_IN_SYMBOL;
-//const int LinesSampler::BARS_IN_SYMBOL;
-//const int LinesSampler::POSSIBLE_SYMBOLS;
-//const int LinesSampler::BARCODE_START_OFFSET;
+const int LinesSampler::MODULES_IN_SYMBOL = 17;
+const int LinesSampler::BARS_IN_SYMBOL = 8;
+const int LinesSampler::POSSIBLE_SYMBOLS = 2787;
+const int LinesSampler::BARCODE_START_OFFSET = 2;
 
 namespace {
 
--- deps/zxing/core/src/zxing/pdf417/detector/LinesSampler.h
+++ deps/zxing/core/src/zxing/pdf417/detector/LinesSampler.h
@@ -28,12 +28,12 @@ namespace detector {
 
 class LinesSampler {
 private:
-  static const int MODULES_IN_SYMBOL = 17;
-  static const int BARS_IN_SYMBOL = 8;
-  static const int POSSIBLE_SYMBOLS = 2787;
+  static const int MODULES_IN_SYMBOL;
+  static const int BARS_IN_SYMBOL;
+  static const int POSSIBLE_SYMBOLS;
   static const std::vector<float> RATIOS_TABLE;
   static std::vector<float> init_ratios_table();
-  static const int BARCODE_START_OFFSET = 2;
+  static const int BARCODE_START_OFFSET;
 
   Ref<BitMatrix> linesMatrix_;
   int symbolsPerLine_;
